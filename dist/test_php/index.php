abaf35d9c5e4b404b082e2cff84216e161effa19
romanproudly <roman@proudly.ru>
2023-07-16 22:08:12 +0300

    test db query

--- dist/test_php/index.php
+++ dist/test_php/index.php
@@ -6,12 +6,14 @@ $db['password'] = 'a4c';
 $db['dbname'] = 'c4a';
 
 $mysqli = new mysqli($db['server'], $db['username'], $db['password'], $db['dbname']);
-if ($mysqli->connect_error) {
-  printf("Cant connect: %s\n", $mysqli -> connect_error);
-  exit();
-};
-
-
-echo 'It works';
+if (!$mysqli->connect_error) {
+	$ip = isset($_SERVER['HTTP_CLIENT_IP']) 
+	    ? $_SERVER['HTTP_CLIENT_IP'] 
+	    : (isset($_SERVER['HTTP_X_FORWARDED_FOR']) 
+		    ? $_SERVER['HTTP_X_FORWARDED_FOR'] 
+		    : $_SERVER['REMOTE_ADDR']);
+	$mysqli->query('INSERT INTO `counters` SET `ip` = ' . $ip . ', `ts` = NOW(), `counter` = 1');
+  	echo 'It works';
+}
 
 ?>
