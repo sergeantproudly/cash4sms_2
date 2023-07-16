6df16a79888a70f2742982a353ed0abef0878475
romanproudly <roman@proudly.ru>
2023-07-16 21:57:43 +0300

    Update index.php

--- dist/test_php/index.php
+++ dist/test_php/index.php
@@ -1,5 +1,17 @@
 <?php
 
+$db['server'] = 'localhost';
+$db['username'] = 'c4a';
+$db['password'] = 'pa4c';
+$db['dbname'] = 'c4a';
+
+$mysqli = new mysqli($db['server'], $db['username'], $db['password'], $db['dbname']);
+if ($mysqli->connect_error) {
+  printf("Cant connect: %s\n", $mysqli -> connect_error);
+  exit();
+};
+
+
 echo 'It works';
 
 ?>
