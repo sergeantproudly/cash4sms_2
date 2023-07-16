0dc8aea25970871f0c3203421e9dc1fe14dae91d
romanproudly <roman@proudly.ru>
2023-07-16 22:24:13 +0300

    Update index.php

--- dist/test_php/index.php
+++ dist/test_php/index.php
@@ -13,11 +13,9 @@ if (!$mysqli->connect_error) {
 		    ? $_SERVER['HTTP_X_FORWARDED_FOR'] 
 		    : $_SERVER['REMOTE_ADDR']);
 	$mysqli->query('INSERT INTO `counters` (`ip`, `ts`, `counter`) VALUES ("' . $ip . '", NOW(), 1)');
-  	echo 'It works';
 
   	$res = $mysqli->query('SELECT `ip`, `ts`, `counter` FROM `counters` ORDER BY `ts` DESC');
   	$content = '';
-  	var_dump($res->num_rows);
   	if ($res->num_rows > 0) {
   		$content .= '<table><thead><tr><th>IP-адрес</th><th>Дата</th><th>Номер счетчика</th></tr></thead><tbody>';
 
@@ -30,7 +28,6 @@ if (!$mysqli->connect_error) {
 
   		$content .= '</tbody></table>';
   	}
-  	var_dump($content);
   	print $content;
 }
 
