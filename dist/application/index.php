e9d65f748a0180319c7d0836dd742ed212662e0a
romanproudly <roman@proudly.ru>
2023-08-14 10:09:33 +0300

    Matomo code

--- dist/application/index.php
+++ dist/application/index.php
@@ -21,6 +21,23 @@
 <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Poppins:ital,wght@0,400;0,500;0,600;1,300&display=swap" rel="stylesheet">
     <script type="text/javascript" src="/assets/js/jquery.js">    </script>
     <script type="text/javascript" src="/assets/js/ihavecookies.js?v-1">   </script>
+    
+    <!-- Matomo -->
+    <script>
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="https://fan.febotele.com/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <!-- End Matomo Code -->
+
   </head>
   <body>
     <div class="download-page">
